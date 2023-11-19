dd82ede3437440d049997141b50d9ead456ef149
adrianmfm <adrianmoisesfls@gmail.com>
2023-11-19 13:51:01 -0300

    vambios

--- estacionamiento-service/index.js
+++ estacionamiento-service/index.js
@@ -1,189 +1,169 @@
 const express = require("express");
 const cors = require("cors");
-const sql = require("mssql");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 app.use(cors());
-const config = {
-    user: "estacionamiento",
-    password: "Est.1234",
-    server: "server-arquitectura.database.windows.net",
-    database: "arquitectura",
-    options : {
-        encrypt : true,
-        trustServerCertificate: true,
-    },
-};
-sql.connect(config, (err) => {
+
+const db = new sqlite3.Database(
+  "../base-de-datos/arquitectura.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
     if (err) {
-        console.error("Error al conectar a la base de datos:", err);
-        return;
+      console.log(err.message);
     }
-    console.log("Conexión a SQL Server exitosa");
-});
+    else{
+      console.log('CONEXION CON EXITO')
+    }
+  }
+);
 
-app.get("/estacionamientos", async (req, res) => {
-    try {
-        const resultados = await new sql.Request().query(
-            "SELECT idPlaza, direccion, tarifa, descripcion, latitud, longitud, disponible  FROM Estacionamiento e INNER JOIN Plaza p ON p.idEstacionamiento = e.idEstacionamiento;"
-        );
-        res.json({ estacionamientos: resultados.recordset });
-    } catch (error) {
-        console.error("Error al ejecutar la consulta:", error);
+app.get("/estacionamientos", (req, res) => {
+  db.all(
+    "SELECT idPlaza, direccion, tarifa, descripcion, latitud, longitud, habilitado FROM Estacionamiento e INNER JOIN Plaza p ON p.idEstacionamiento = e.idEstacionamiento;",
+    [],
+    (err, rows) => {
+      if (err) {
+        console.error("Error al ejecutar la consulta:", err);
         res.status(500).send("Error interno del servidor");
+        return;
+      }
+      res.json({ estacionamientos: rows });
     }
+  );
 });
 
-app.get("/estacionamientos", async (req, res) => {
-    try {
-      const resultados = await new sql.Request().query(
-        "SELECT idPlaza, direccion, tarifa, descripcion, latitud, longitud, disponible  FROM Estacionamiento e INNER JOIN Plaza p ON p.idEstacionamiento = e.idEstacionamiento;"
-      );
-      res.json({ estacionamientos: resultados.recordset });
-    } catch (error) {
-      console.error("Error al ejecutar la consulta:", error);
-      res.status(500).send("Error interno del servidor");
+app.get("/datos-personales", (req, res) => {
+  db.all("SELECT * FROM Persona", [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
     }
+    res.json({ datosPersonales: rows });
   });
-  
-  const puerto = 3001;
-  app.listen(puerto, () => {
-    console.log(`El servidor está corriendo en http://localhost:${puerto}`);
-  });
-  
-  app.get("/datos-personales", async (req, res) => {
-    try {
-      const resultados = await new sql.Request().query("SELECT * FROM Persona");
-      res.json({ datosPersonales: resultados.recordset });
-    } catch (error) {
-      console.error("Error al ejecutar la consulta:", error);
+});
+
+app.get("/datos-personales/:id", (req, res) => {
+  const id = req.params.id;
+  db.get(`SELECT * FROM Persona WHERE idPersona = ${id}`, [], (err, row) => {
+    if (err) {
+      console.error("Error al ejecutar la consulta:", err);
       res.status(500).send("Error interno del servidor");
+      return;
     }
+    res.json({ datosPersonales: row });
   });
-  
-  app.get("/datos-personales/:id", async (req, res) => {
-    try {
-      const id = req.params.id;
-      const resultados = await new sql.Request().query(
-        `SELECT * FROM Persona WHERE idPersona = ${id}`
-      );
-      res.json({ datosPersonales: resultados.recordset[0]});
-    } catch (error) {
-      console.error("Error al ejecutar la consulta:", error);
-      res.status(500).send("Error interno del servidor");
-    }
-  });
-  
-  app.get("/plaza/:idPlaza/disponible", async (req, res) => {
-    try {
-      const idPlaza = req.params.idPlaza;
-      const result = await new sql.Request().query(`
-      SELECT disponible
-      FROM Estacionamiento e
-      INNER JOIN Plaza p
-      ON p.idEstacionamiento = e.idEstacionamiento
-      WHERE p.idPlaza = ${idPlaza}`);
-      if (result.recordset.length === 0) {
+});
+
+app.get("/plaza/:idPlaza/habilitado", (req, res) => {
+  const idPlaza = req.params.idPlaza;
+  db.get(
+    `SELECT habilitado FROM Estacionamiento e INNER JOIN Plaza p ON p.idEstacionamiento = e.idEstacionamiento WHERE p.idPlaza = ${idPlaza}`,
+    [],
+    (err, row) => {
+      if (err) {
+        console.error("Error al verificar disponibilidad de la plaza:", err);
+        res.status(500).send("Error interno del servidor");
+        return;
+      }
+      if (!row) {
         res.status(404).json({ mensaje: "Plaza no encontrada" });
         return;
       }
-      const disponibilidad = result.recordset[0].disponible;
-      res.json({ disponible: disponibilidad });
-    } catch (error) {
-      console.error("Error al verificar disponibilidad de la plaza:", error);
-      res.status(500).send("Error interno del servidor");
+      res.json({ habilitado: row.habilitado });
     }
-  });
-  app.post("/plaza/:idPlaza/arrendar/:idPersona", async (req, res) => {
-    try {
-      const idPlaza = req.params.idPlaza;
-      const idPersona = req.params.idPersona;
-  
-      // Actualiza la disponibilidad de la plaza en tu base de datos
-      await new sql.Request().query(
-        `UPDATE Plaza SET disponible = 0, idPersona=${idPersona} WHERE idPlaza = ${idPlaza}`
-      );
-  
+  );
+});
+
+app.post("/plaza/:idPlaza/arrendar/:idPersona", (req, res) => {
+  const idPlaza = req.params.idPlaza;
+  const idPersona = req.params.idPersona;
+  db.run(
+    `UPDATE Plaza SET habilitado = 0, idPersona = ${idPersona} WHERE idPlaza = ${idPlaza}`,
+    [],
+    (err) => {
+      if (err) {
+        console.error("Error al arrendar la plaza:", err);
+        res.status(500).send("Error interno del servidor");
+        return;
+      }
       res.json({ mensaje: "Plaza arrendada con éxito" });
-    } catch (error) {
-      console.error("Error al arrendar la plaza:", error);
-      res.status(500).send("Error interno del servidor");
     }
-  });
-  
-  app.get("/arriendos", async (req, res) => {
-    try {
-      const result = await new sql.Request().query(`
-        SELECT a.*
-        FROM Arriendo a
-        INNER JOIN Persona p ON a.idPersona = p.idPersona
-      `);
-  
-      res.json({ arriendos: result.recordset });
-    } catch (error) {
-      console.error("Error al obtener los arriendos:", error);
-      res.status(500).send("Error interno del servidor");
+  );
+});
+
+app.get("/arriendos", (req, res) => {
+  db.all(
+    `SELECT a.*
+     FROM Arriendo a
+     INNER JOIN Persona p ON a.idPersona = p.idPersona`,
+    [],
+    (err, rows) => {
+      if (err) {
+        console.error("Error al obtener los arriendos:", err);
+        res.status(500).send("Error interno del servidor");
+        return;
+      }
+      res.json({ arriendos: rows });
     }
-  });
-  
-  app.get("/contar-plazas", async (req, res) => {
-    try {
-      const result = await new sql.Request().query(`
-      SELECT COUNT(*) as cantidadPlazas
-      FROM Plaza p
-      INNER JOIN Estacionamiento e ON p.idEstacionamiento = e.idEstacionamiento
-      WHERE p.disponible = 1
-      `);
-  
-      res.json({ arriendos: result.recordset[0].cantidadPlazas });
-    } catch (error) {
-      console.error("Error al obtener los arriendos:", error);
-      res.status(500).send("Error interno del servidor");
+  );
+});
+
+app.get("/contar-plazas", (req, res) => {
+  db.get(
+    `SELECT COUNT(*) as cantidadPlazas
+     FROM Plaza p
+     INNER JOIN Estacionamiento e ON p.idEstacionamiento = e.idEstacionamiento
+     WHERE p.habilitado = 1`,
+    [],
+    (err, row) => {
+      if (err) {
+        console.error("Error al obtener la cantidad de plazas:", err);
+        res.status(500).send("Error interno del servidor");
+        return;
+      }
+      res.json({ cantidadPlazas: row.cantidadPlazas });
     }
-  });
-  
-  app.get("/info-estacionamientos/:idPersona", async (req, res) => {
-    try {
-      const idPersona = req.params.idPersona;
-  
-      const pool = await sql.connect(config);
-      const result = await pool.request()
-        .query(`SELECT e.descripcion, e.direccion, e.tarifa, p.alto, p.ancho, p.largo
-                FROM Estacionamiento e
-                INNER JOIN Plaza p ON e.idEstacionamiento = p.idEstacionamiento
-                INNER JOIN Persona per ON per.idPersona = p.idPersona
-                WHERE per.idPersona = ${idPersona}`);
-  
-      res.json({ infoEstacionamiento: result.recordset });
-    } catch (error) {
-      console.error(
-        "Error al obtener la información del estacionamiento:",
-        error
-      );
-      res.status(500).send("Error interno del servidor");
+  );
+});
+
+app.get("/info-estacionamientos/:idPersona", (req, res) => {
+  const idPersona = req.params.idPersona;
+  db.all(
+    `SELECT e.descripcion, e.direccion, e.tarifa, p.alto, p.ancho, p.largo
+     FROM Estacionamiento e
+     INNER JOIN Plaza p ON e.idEstacionamiento = p.idEstacionamiento
+     INNER JOIN Persona per ON per.idPersona = p.idPersona
+     WHERE per.idPersona = ${idPersona}`,
+    [],
+    (err, rows) => {
+      if (err) {
+        console.error("Error al obtener la información del estacionamiento:", err);
+        res.status(500).send("Error interno del servidor");
+        return;
+      }
+      res.json({ infoEstacionamiento: rows });
     }
-  });
-  
-  app.post("/liberar-plaza/:idPlaza", async (req, res) => {
-    //libera todas las plazas
-    const idPlaza = req.params.idPlaza;
-  
-    try {
-      const pool = await sql.connect(config);
-      const result = await pool
-        .request()
-        .input("idPlaza", sql.Int, idPlaza)
-        .query(`UPDATE Plaza SET disponible = 1, idPersona = null`);
-  
+  );
+});
+
+app.post("/liberar-plaza/:idPlaza", (req, res) => {
+  const idPlaza = req.params.idPlaza;
+  db.run(
+    `UPDATE Plaza SET habilitado = 1, idPersona = null WHERE idPlaza = ${idPlaza}`,
+    [],
+    (err) => {
+      if (err) {
+        console.error("Error al liberar la plaza:", err);
+        res.status(500).send("Error interno del servidor");
+        return;
+      }
       res.json({ mensaje: "Plaza liberada con éxito" });
-    } catch (error) {
-      console.error("Error al liberar la plaza:", error);
-      res.status(500).send("Error interno del servidor");
     }
-  });
-  
-  process.on("SIGINT", () => {
-    sql.close(); // Cierra la conexión cuando se detiene la aplicación
-    process.exit();
-  });
-  
+  );
+});
+
+const puerto = 3000;
+app.listen(puerto, () => {
+  console.log(`El servidor está corriendo en http://localhost:${puerto}`);
+});
